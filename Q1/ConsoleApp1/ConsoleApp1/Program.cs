91eddb26166e5fd412b3be4ac27a9e9ecb310d43
Klevelandranapia30019853 <48032182+Klevelandranapia30019853@users.noreply.github.com>
2019-05-16 12:09:14 +1200

    Add files via upload

---
+++ Q1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this program can give the result of base^exponent");
+            Console.ReadLine();
+
+            Console.WriteLine("enter the value of the base");
+            Console.ReadLine();
+         
+            Console.WriteLine("enter the value of the exponent");
+            Console.ReadLine();
+
+            Console.WriteLine(GetPow(2, 5));
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for(int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ Q2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("-this program will give the sum total of 5 prices-");
+            Console.WriteLine("--------------------------------------------------");
+            Console.ReadLine();
+
+
+            static int GetPow(int baseNum, int powNum)
+            {
+                int result = 1;
+
+                for (int i = 0; i < powNum; i++)
+                {
+                    result = result * baseNum;
+                }
+
+                return result;
+            }
+        }    
+    }
+}
